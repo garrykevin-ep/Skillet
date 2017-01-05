06861b19198d3fb6b4164c7487196616e8901af4
garrykevin-ep <garrykevin.ep@gmail.com>
2017-01-05 20:13:43 +0530

    added a register option for a user

--- login/views.py
+++ login/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate,login
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,HttpResponse
 from django.urls import reverse
+from django.contrib.auth.models import User
 #user defined
 from .models import UserProfile
+from .form import RegisterForm
 
 
 
@@ -21,3 +23,37 @@ def auth(request):
     else:
         return render(request,'login/login.html',{'error_message' : "Wrong Pass",
                                                   'x' : pasword})
+def register(request):
+    if request.method == 'POST':
+        form  = RegisterForm(request.POST)
+        if form.is_valid(): #need to check if user alredy exist NEED TO 
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            
+            #if user not exist create a user
+            user = User.objects.create_user(username = username)
+            user.set_password(password)
+            user.save()
+            
+            #save phone number of a user 
+            number = form.cleaned_data['phone']
+            ph_save(user,number)
+            
+            #login and redirect to main page
+            auth = authenticate(username = username ,password = password)
+            login(request,auth)
+            return HttpResponseRedirect(reverse('quiz:index',args = (1,)))
+        else:
+            #when form data is wrong
+            form = RegisterForm()
+            return render(request,'login/register.html',{'error_message' : "not vaild username",'form' : form})   
+    else:
+        #if just display page
+        form = RegisterForm()
+        return render(request,'login/register.html', {'form' : form})
+
+#save phone number of a user
+def ph_save(user,number):
+    usr_pro = UserProfile.objects.get(user = user.id)
+    usr_pro.ph_no = number
+    usr_pro.save()
