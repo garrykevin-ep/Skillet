a77a645bd5a92a9a6717ed9b66e905ef4cd20978
garrykevin <garrykevin.ep@gmail.com>
2017-01-15 10:15:33 +0000

    added meaning full questions

--- login/form.py
+++ login/form.py
@@ -5,6 +5,6 @@ from django.utils.translation import ugettext as _
 
 class RegisterForm(forms.Form):
 	username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
-	phone = forms.CharField(widget=forms.TextInput(attrs=dict(required=True)),  label=_("Phone Number"))
-	password1 = forms.IntegerField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
+	phone = forms.IntegerField(widget=forms.TextInput(attrs=dict(required=True)),  label=_("Phone Number"))
+	password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
 	#password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password (again)"))
