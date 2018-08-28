71c82c331916f52960fb17313b2ade50980227a4
garrykevin <garrykevin.ep@gmail.com>
2018-08-28 23:31:46 +0530

    test objects handled due to remove choose test

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,10 +5,17 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from quiz.views import first_question
 from quiz.models import TestStatus , Test
+from django.db import IntegrityError
 # from .config import *
 
 # Create your views here.
 @login_required(login_url = '/')
 def dashboard(request):
-	test_statuses = TestStatus.objects.filter(user = request.user,completed = False)	
+	tests = Test.objects.filter(publish=True)
+	for test in tests:
+		try:
+			TestStatus.objects.create(user=request.user,test=test,minute=test.minute,second=test.second)
+		except IntegrityError:
+			pass
+	test_statuses = TestStatus.objects.filter(user = request.user,completed = False,test__publish=True)	
 	return render(request,'dashboard/index.html',{"test_statuses" : test_statuses})
